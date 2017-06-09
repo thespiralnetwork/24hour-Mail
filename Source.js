f77939d23999e540c770b2fe30801c1f3c5f7a40
thespiralnetwork <thespiralnetwork@icloud.com>
2017-06-10 09:54:38 +1000

    Added The Source

---
+++ Source.js
@@ -0,0 +1,22 @@
+// Do Not Remove This And What Under
+// Made By SubZakk, SuperSUgamer, TheSpiralNetwork (Or Chris)
+// Version 2.0 Is In The Source And Its Over 9000% Faster Than The Hosted Version
+// Do Not Remove Whats Above
+
+
+
+var i = 1;                     
+
+function LoopMail () {           
+   setTimeout(function () {    
+      resetSessionLife()         
+      i++;                     
+      if (i < 9999999999999999) {            
+         LoopMail();             
+      }                        
+   }, 3000)
+}
+LoopMail()
+
+
+// Read ReadME First
